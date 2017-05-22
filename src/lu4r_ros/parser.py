c5e14d0cfcb41c9aa7dc662a7269a5e9f06eff56
TheDaringDan <ds1267103@gmail.com>
2017-05-22 19:37:25 -0400

    Finalized first iteration of the LU4R parser:
     - Reads one line (sentence) and returns a list of Lu4r objects
     - Return causes a rospy.ServiceException - Invalid number of arguments (for some reason)
     - An empty item needs to be added to mitigate a bug where python does not recognize a list with 1 item as a list
     - Added a args.msg object and modified Lu4r.msg, cakkin_make install, source devel/setup.bash and catkin_make is recommended

--- src/lu4r_ros/parser.py
+++ src/lu4r_ros/parser.py
@@ -9,7 +9,7 @@ import socket
 import sys
 import requests
 import json
-
+import re
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -27,17 +27,43 @@ def parse_goal_server():
 
 def handle_goal(req):
     lu4r_ip = rospy.get_param("~lu4r_ip", '127.0.0.1')
-    lu4r_port = rospy.get_param('~lu4r_port', '1000')
-    lu4r_url = 'http://' + lu4r_ip + ':' + str(lu4r_port) + '/service/nlu'
+    lu4r_port = rospy.get_param('~lu4r_port', '9001')
+    lu4r_url = 'http://' + str(lu4r_ip) + ':' + str(lu4r_port) + '/service/nlu'
 
-    lu4r_json(req.sentence, lu4r_url)
+    r = lu4r_json(req.sentence, lu4r_url)
+    lu4rList = []
+    lu4rItem = Lu4r()
+    lu4rList.append(lu4rItem)
 
+    for line in r.iter_lines():
+        if line:
+            print line
+            operation = re.compile('(\s*:op\d\s)?\([a-zA-Z0-9\-]+ / ([a-zA-Z\-]+)')
+            m = operation.match(line)
+            if m:
+                if lu4rItem.action != "" and lu4rItem.action != 'and':
+                    lu4rList.append(lu4rItem)
+                lu4rItem = Lu4r()
+                lu4rItem.action = m.group(2)
+            else:
+                action = re.compile('\s*:([a-zA-Z0-9]*) \([a-zA-Z0-9]* / ([a-zA-Z0-9]*)\)')
+                n = action.match(line)
+                if n:
+                    lu4rItem.args.append((n.group(1), n.group(2)))
+
+    if lu4rItem.action != "":
+        lu4rList.append(lu4rItem)
+
+    print lu4rList
+
+    return lu4rList
 
 def lu4r_json(sentence, url):
-    to_send = {'hypo': "Go get the book on the shelf", 'entities': [{"atom":"book1","type":"book","preferredLexicalReference":"book","alternativeLexicalReferences":["volume","manual","dictionary","text"],"coordinate":{"x":"13.0","y":"0.0","z":"0.0","angle":"0"}},{"atom":"table1","type":"table","preferredLexicalReference":"table","alternativeLexicalReferences":["counter","desk"],"coordinate":{"x":"16.0","y":"0.0","z":"0.0","angle":"0.671"}},{"atom":"glass1","type":"glass","preferredLexicalReference":"glass","alternativeLexicalReferences":["cup","decanter","chalice"],"coordinate":{"x":"5.0","y":"8.0","z":"0.0","angle":"0"}},{"atom":"bedroom1","type":"bedroom","preferredLexicalReference":"bedroom","alternativeLexicalReferences":["chamber","cubicle","bedchamber"],"coordinate":{"x":"11.0","y":"6.0","z":"0.0","angle":"0"}},{"atom":"studio1","type":"studio","preferredLexicalReference":"studio","alternativeLexicalReferences":["library","office"],"coordinate":{"x":"9.0","y":"14.0","z":"0.0","angle":"0"}},{"atom":"person1","type":"person","preferredLexicalReference":"person","alternativeLexicalReferences":["body","character","guy","man","woman"],"coordinate":{"x":"2.0","y":"2.0","z":"0.0","angle":"0"}}]}
-    r = requests.post(url, to_send, headers=HEADERS)
-    print r
+    HYPO = {'hypotheses':[{"transcription":sentence,"confidence":"0.9","rank":"1"}]}
+    ENT = {'entities': [{"atom":"book1","type":"book","preferredLexicalReference":"book","alternativeLexicalReferences":["volume","manual","dictionary","text"],"coordinate":{"x":"13.0","y":"0.0","z":"0.0","angle":"0"}},{"atom":"table1","type":"table","preferredLexicalReference":"table","alternativeLexicalReferences":["counter","desk"],"coordinate":{"x":"16.0","y":"0.0","z":"0.0","angle":"0.671"}},{"atom":"glass1","type":"glass","preferredLexicalReference":"glass","alternativeLexicalReferences":["cup","decanter","chalice"],"coordinate":{"x":"5.0","y":"8.0","z":"0.0","angle":"0"}},{"atom":"bedroom1","type":"bedroom","preferredLexicalReference":"bedroom","alternativeLexicalReferences":["chamber","cubicle","bedchamber"],"coordinate":{"x":"11.0","y":"6.0","z":"0.0","angle":"0"}},{"atom":"studio1","type":"studio","preferredLexicalReference":"studio","alternativeLexicalReferences":["library","office"],"coordinate":{"x":"9.0","y":"14.0","z":"0.0","angle":"0"}},{"atom":"person1","type":"person","preferredLexicalReference":"person","alternativeLexicalReferences":["body","character","guy","man","woman"],"coordinate":{"x":"2.0","y":"2.0","z":"0.0","angle":"0"}}]}
+    r = requests.post(url, data={'hypo':str(HYPO) , 'entities':str(ENT)}, headers=HEADERS)
 
+    return r
 
 if __name__ == '__main__':
     parse_goal_server()
